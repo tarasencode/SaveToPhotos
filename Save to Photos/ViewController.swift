717225d53fc7b893772f4d5c3cf25a02002b5f62
oleG <oleg@olegs-imac.local>
2020-05-04 10:03:20 +0300

    Initial Commit

---
+++ Save to Photos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Save to Photos
+//
+//  Created by oleG on 04/05/2020.
+//  Copyright © 2020 oleG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
